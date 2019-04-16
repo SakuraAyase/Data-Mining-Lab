ee7a898831df2a26817a74e24b906e06bbd7a811
Yi Jiang <2408409697@qq.com>
2019-04-16 17:58:01 +0800

    Add files via upload

---
+++ Basic Classifier/KNN.py
@@ -0,0 +1,46 @@
+from sklearn.neighbors import KNeighborsClassifier
+import load_data
+
+
+def define_model():
+    model = KNeighborsClassifier()
+    return model
+
+def get_acc(fake,true):
+    acc = 0
+
+
+    for i in range(len(fake)):
+        if (fake[i] == true[i]):
+            acc = acc + 1
+    acc = acc / len(fake)
+
+    return acc
+
+def data_process():
+    data = load_data.load()[0]
+    label = load_data.load()[1]
+
+    train_size = int(0.8*data.shape[0])
+    train_data = data[:train_size,:]
+    test_data = data[train_size:, :]
+    train_label = label[:train_size,:]
+    test_label = label[train_size:,:]
+
+    return train_data,train_label,test_data,test_label
+
+
+def train():
+    train_data, train_label, test_data, test_label = data_process()
+    model = define_model()
+    model.fit(train_data,train_label)
+
+    fake_label_test = model.predict(test_data)
+    fake_label_train = model.predict(train_data)
+
+    return get_acc(fake_label_test,test_label),get_acc(fake_label_train,train_label)
+
+
+result = train()
+print("acc in testing set = "+ str(result[0]))
+print("acc in training set = "+ str(result[1]))
