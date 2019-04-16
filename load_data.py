6fcdcb40298be8ffd3c467d2dc7f12abea39344c
Yi Jiang <2408409697@qq.com>
2019-04-16 15:30:49 +0800

    Add files via upload

---
+++ load_data.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy  as np
+
+
+
+def load_data(path = 'segment.dat'):
+
+    data = pd.read_table(path,header=None,sep='\s+')
+    data =  data.values
+    return data
+
+def segment_label_data(dataset):
+    dataset = np.array(dataset)
+    label = dataset[:, -1:]
+    data = dataset[:, :-1]
+    return data,label
+
+def load():
+    return segment_label_data(load_data())
